09d9962aa9e2772c9eaa7dc845e9eaf5c57d5ea4
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2023-01-15 23:24:18 +0330

    add js code for interest section

--- js/main.js
+++ js/main.js
@@ -10,3 +10,20 @@ openModalBtn.addEventListener("click", () => {
 closeModalBtn.addEventListener("click", () => {
   modal.classList.remove("open");
 });
+
+// Interests
+function showContent(event, tabName) {
+  const tabLinks = document.querySelectorAll(".tabLink");
+  const tabContents = document.querySelectorAll(".tabContent");
+
+  tabLinks.forEach((tabLink) => {
+    tabLink.classList.remove("active");
+  });
+
+  tabContents.forEach((tabContent) => {
+    tabContent.classList.remove("active");
+  });
+
+  event.currentTarget.classList.add("active");
+  document.getElementById(tabName).classList.add("active");
+}
