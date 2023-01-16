6cbd5e697ce3ca0cdd574e17f4c6a4473685ef78
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2023-01-16 19:41:23 +0330

    add show more/less button in skill section

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ closeModalBtn.addEventListener("click", () => {
   modal.classList.remove("open");
 });
 
-// Interests
+// Interest section
 function showContent(event, tabName) {
   const tabLinks = document.querySelectorAll(".tabLink");
   const tabContents = document.querySelectorAll(".tabContent");
@@ -27,3 +27,33 @@ function showContent(event, tabName) {
   event.currentTarget.classList.add("active");
   document.getElementById(tabName).classList.add("active");
 }
+
+// Show more and show less in skill section
+const skillFooter = document.querySelector(".skill__footer");
+const skillFooterText = document.querySelector(".skill__footerText");
+const skillFooterIcon = document.querySelector(
+  ".skill__footer .fa-chevron-down"
+);
+
+const skillHidden = document.querySelectorAll(".skill__hidden");
+const skillShown = document.querySelectorAll(".skill__shown");
+
+skillFooter.addEventListener("click", (e) => {
+  if (skillFooterText.innerHTML === "Show all skills") {
+    skillFooterText.innerHTML = "Show less";
+    skillFooterIcon.style.transform = "rotate(-180deg)";
+  } else {
+    skillFooterText.innerHTML = "Show all skills";
+    skillFooterIcon.style.transform = "rotate(0deg)";
+  }
+
+  skillHidden.forEach((item) => {
+    item.classList.toggle("skill__shown");
+    item.classList.toggle("skill__hidden");
+  });
+
+  skillShown.forEach((item) => {
+    item.classList.toggle("skill__shown");
+    item.classList.toggle("skill__hidden");
+  });
+});
